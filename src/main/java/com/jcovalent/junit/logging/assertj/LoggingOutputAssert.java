9f8a3854ed3832e31c8f12e3d84f05eda287bd9c
Jeromy Cannon <jeromy@swirldslabs.com>
2023-07-07 15:54:47 -0500

    assertThatLogEntriesHaveMessages
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- src/main/java/com/jcovalent/junit/logging/assertj/LoggingOutputAssert.java
+++ src/main/java/com/jcovalent/junit/logging/assertj/LoggingOutputAssert.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 JCovalent
+ * Copyright (C) 2023 JCovalent
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,11 @@
  */
 package com.jcovalent.junit.logging.assertj;
 
+import com.jcovalent.junit.logging.LogEntry;
 import com.jcovalent.junit.logging.LoggingOutput;
+import java.util.List;
 import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.Condition;
 
 public class LoggingOutputAssert extends AbstractAssert<LoggingOutputAssert, LoggingOutput> {
 
@@ -26,5 +29,21 @@ public class LoggingOutputAssert extends AbstractAssert<LoggingOutputAssert, Log
 
     public static LoggingOutputAssert assertThat(final LoggingOutput actual) {
         return new LoggingOutputAssert(actual);
+    }
+
+    /**
+     * Asserts that the given log entries contain the expected log entries only matching the level and message.
+     * @param actual the loggingOutput to check
+     * @param expectedLogEntries the expected log entries
+     */
+    public static void assertThatLogEntriesHaveMessages(final LoggingOutput actual, List<LogEntry> expectedLogEntries) {
+        org.assertj.core.api.Assertions.assertThat(actual.allEntries()).isNotNull();
+        for (LogEntry logEntry : expectedLogEntries) {
+            org.assertj.core.api.Assertions.assertThat(actual.allEntries())
+                    .haveAtLeastOne(new Condition<>(
+                            entry -> entry.level() == logEntry.level()
+                                    && entry.message().contains(logEntry.message()),
+                            "message contains '" + logEntry.message() + "'"));
+        }
     }
 }
